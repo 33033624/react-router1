539406260eb3c0cb75e01b28791c3e1957f640eb
33033624@qq.com <330336214@qq.com>
2017-02-22 22:48:23 +0800

    delete node_modules

--- node_modules/react-tappable/src/Tappable.js
+++
@@ -1,9 +0,0 @@
-var TappableMixin = require('./TappableMixin');
-var getComponent = require('./getComponent');
-var touchStyles = require('./touchStyles');
-
-var Component = getComponent([TappableMixin]);
-
-module.exports = Component;
-module.exports.touchStyles = touchStyles;
-module.exports.Mixin = TappableMixin;
